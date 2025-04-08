9d20f53d84505186f2b74e0e211e22b2c8d7d945
JUNAID <moinuddinm942@gmail.com>
2025-04-08 20:58:28 +0530

    code updated

---
+++ ARRAYS/9 .Minimize the Heights II.py
@@ -0,0 +1,26 @@
+def getMinDiff(self, arr,k):
+        
+        n = len(arr)
+        arr.sort()
+        big=arr[-1]
+        small=arr[0]
+    
+        #initial result after sorting
+        res = big - small
+    
+        
+        for i in range(1, len(arr)):
+            # Impossible to decrement height of ith tower by k, 
+            # continue to the next tower
+            if arr[i] - k < 0:
+                continue
+            
+            # For all indices i, increment arr[0...i-1] by k and
+            # decrement arr[i...n-1] by k
+            minH = min(small + k, arr[i] - k)
+    
+            maxH = max(big - k,arr[i - 1] + k)
+    
+            res = min(res, maxH - minH)
+        
+        return res
