c83602d5e44427d4860847cecbc15f86553096b4
JUNAID <moinuddinm942@gmail.com>
2025-05-03 22:57:58 +0530

    code updated

--- SORTING/28 .Number of occurrence.py
+++ SORTING/28 .Number of occurrence.py
@@ -3,3 +3,44 @@ class Solution:
         #code here
         f=arr.count(target)
         return f
+#----OR----
+#use binary search to find the first and last occurrence of the target element in the array.
+
+def lowerbound(arr, target):
+    res=len(arr)
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + (high-low)) // 2
+        if arr[mid] >= target:
+            res=mid
+            high = mid - 1
+            
+        else:
+            low = mid + 1
+    return res
+
+def higherbound(arr, target):
+    res=len(arr)
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + (high-low)) // 2
+        if arr[mid] > target:
+            res=mid
+            high = mid - 1
+            
+        else:
+            low = mid + 1
+    return res
+
+def countFreq(arr, target):
+    first = lowerbound(arr, target)
+    last = higherbound(arr, target)
+    return last - first 
+
+#example usage
+arr = [1, 2, 2, 2, 3, 4, 5]
+target = 2
+ans=countFreq(arr, target)                  # Output: 3
+print(ans)
