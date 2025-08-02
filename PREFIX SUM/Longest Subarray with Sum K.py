87250ddb44ea99eea25a1f5300d32e366881b5c3
JUNAID <moinuddinm942@gmail.com>
2025-08-02 23:44:54 +0530

    add implementation for finding the longest subarray with sum k

---
+++ PREFIX SUM/Longest Subarray with Sum K.py
@@ -0,0 +1,22 @@
+def maxLen(arr,k):
+    seen={}     #dictionary to store the sum and index
+    seen[0]=-1   #to handle the case when subarray starts from index
+    sum=0        
+    maxlen=0
+    
+    for i,num in enumerate(arr):
+        sum+=num
+        
+        if sum==k:
+            maxlen=max(maxlen,i+1)    #maxlen is i+1 if sum equals k
+        elif sum-k in seen:
+            maxlen=max(maxlen,i-seen[sum-k])
+        else:
+            seen[sum]=i             #store the index of sum-k
+    return maxlen
+    
+    
+# Example usage:
+arr=[94, -33, -13, 40, -82, 94, -33, -13, 40, -82]
+k=52
+print(maxLen(arr,k))  
